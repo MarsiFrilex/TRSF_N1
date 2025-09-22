b4c26780ebfdce2bf14af6847dbd0a9ca910ae14
MarsiFrilex <tokalexandra17@gmail.com>
2025-09-22 10:38:55 +0300

    Init main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,14 @@
+import asyncio
 
+import uvicorn
+
+from fastapi import FastAPI
+
+from src.api.base_router import base_router
+from src.database.connection import delete_tables, create_tables
+
+app = FastAPI()
+app.include_router(base_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
