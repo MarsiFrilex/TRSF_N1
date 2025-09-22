77f33592922887e1372ebb17745d660358c9905f
MarsiFrilex <tokalexandra17@gmail.com>
2025-09-22 14:33:20 +0300

    Init router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import LoginPage from "@/pages/LoginPage.vue";
+import ObjectsPage from "@/pages/ObjectsPage.vue";
+import DefectsPage from "@/pages/DefectsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+      {
+          path: '/',
+          name: 'Objects',
+          component: ObjectsPage,
+      },
+      {
+          path: '/login',
+          name: 'Login',
+          component: LoginPage,
+      },
+      {
+          path: '/defects',
+          name: 'Defects',
+          component: DefectsPage,
+      }
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    // Тут будет логика переброса на страницу входа, если не зареган
+    next();
+});
+
+export default router;
