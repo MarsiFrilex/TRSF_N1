5d59c8b4dd6c8b955a50878f37ed3e376f0aeec3
MarsiFrilex <tokalexandra17@gmail.com>
2025-10-08 13:15:17 +0300

    Add CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,12 +3,21 @@ import asyncio
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.base_router import base_router
 from src.database.connection import delete_tables, create_tables
 
 app = FastAPI()
 app.include_router(base_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
